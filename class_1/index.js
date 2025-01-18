e9dcb0ec458f8e73e751176a0ce880679f93ae17
rupam <rupamdey#gmail.com>
2025-01-18 19:42:59 +0530

    class_1

---
+++ class_1/index.js
@@ -0,0 +1,19 @@
+function add(num1,num2){
+    return num1+num2;
+}
+console.log(add(2,3));
+
+//above code are example of fun declaration
+
+let funname=function addThree(num1,num2){
+    return num1+num2;
+}
+console.log(funname(2,3));
+
+//above code are example of fun expression
+
+let anoFun=function (num1,num2){
+    return num1+num2;
+}
+console.log(anoFun(2,3));
+//above code are example of fun expression+anonymous fun
